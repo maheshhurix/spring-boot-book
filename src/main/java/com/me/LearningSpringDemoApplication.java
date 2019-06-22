e72d2546e281fb29527a68f96250b6852a4a46f3
maheshhurix <50582441+maheshhurix@users.noreply.github.com>
2019-06-23 02:23:40 +0530

    Add files via upload

---
+++ src/main/java/com/me/LearningSpringDemoApplication.java
@@ -0,0 +1,13 @@
+package com.me;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningSpringDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningSpringDemoApplication.class, args);
+	}
+
+}
